77ae44ea9da37e095ebf684c428fb02a6ab91ce6
AllBitsEqual <allbitsequal@gmail.com>
2023-07-13 22:49:29 +0200

    moved components and constants to src/ and adjuster alias in tsconfig

--- src/components/EditScreenInfo.tsx
+++ src/components/EditScreenInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
-import { ExternalLink } from './ExternalLink';
+import { ExternalLink } from '@/components/ExternalLink';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
