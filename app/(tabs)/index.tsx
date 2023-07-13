ac22575a7aba5558338569c38ce1ef1fa7ee4744
AllBitsEqual <allbitsequal@gmail.com>
2023-07-13 23:37:10 +0200

    added gluestack provider and one example page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,12 @@
-import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Center, Divider, Text } from '@/gluestack'
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <Center flex={1}>
+      <Text fontSize="$lg" fontWeight='bold'>Tab One</Text>
+      <Divider my={30} w="$4/5" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    </Center>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
+import { GluestackUIProvider } from '@/gluestack'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { config } from '../gluestack-ui.config'
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -46,11 +48,13 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
+    <GluestackUIProvider config={config.theme}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
